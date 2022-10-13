44a805caf469152a27801578f3f47ec06aaac3b9
edarcode <edarcode@gmail.com>
2022-10-12 22:23:50 -0500

    feat/ metodo validateDto ahora valida keys requeridas

--- src/utils/validate-dto/validateKeysDto.ts
+++ src/utils/validate-dto/validateKeysDto.ts
@@ -4,23 +4,33 @@ import { calcTypeof } from "./calcTypeof";
 
 export const validateKeysDto = (dto: object, rules: Rules[]) => {
 	const arrDto = Object.entries(dto);
+	const copyRules = [...rules];
+	const arrKeysDto = [];
 
 	for (let i = 0; i < arrDto.length; i++) {
 		const [keyDto, valueDto] = arrDto[i];
-		const ruleDto = rules.find(rule => rule.key === keyDto);
+		arrKeysDto.push(keyDto);
+		const ruleDto = copyRules.find(rule => rule.key === keyDto);
 
 		const ERR_KEY = `${keyDto} no valida`;
-		if (!ruleDto || !valueDto)
+		if (!ruleDto || valueDto === "")
 			throw new CustomError({ message: ERR_KEY, status: 400 });
 
 		const ERR_TYPE = `${ruleDto.key} debe ser de tipo ${ruleDto.type}`;
+		if (calcTypeof(valueDto) !== ruleDto.type)
+			throw new CustomError({
+				message: ruleDto.msg || ERR_TYPE,
+				status: ruleDto.status || 400
+			});
+	}
 
-		if (ruleDto.required) {
-			if (calcTypeof(valueDto) !== ruleDto.type)
-				throw new CustomError({
-					message: ruleDto.msg || ERR_TYPE,
-					status: ruleDto.status || 400
-				});
-		}
+	for (let i = 0; i < copyRules.length; i++) {
+		const rule = copyRules[i];
+		const REQUIRED_KEY = `Falta el ${rule.key}`;
+		if (rule.required && !arrKeysDto.includes(rule.key))
+			throw new CustomError({
+				message: rule.msg || REQUIRED_KEY,
+				status: rule.status || 400
+			});
 	}
 };
